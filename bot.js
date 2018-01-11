33022844abb670ecc54f43cd133f1fa6ad4943d0
cerimaKoini <cerima.koini@gmx.at>
2018-01-11 01:50:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -84,7 +84,16 @@ client.on('ready', () => {
 	var currentDay = new Date(Date.now()).getDate();
 	var currentMonth = new Date(Date.now()).getMonth()+1;
 	
-	
+	// Creating a new job
+	var job1 = new cron.CronJob({
+		cronTime: '55 1 11 0 *',
+		onTick: function() {
+			var channel = client.channels.find('name', 'bot-spielwiese');
+			channel.send("<@219918618278756352>, alles Gute zum Geburtstag.");
+		},
+		start: false
+	});
+	job1.start();
 	
 	
 //	for (i = 0; i < birthdays.length; i++){
@@ -114,17 +123,6 @@ client.on('message', message => {
 		    	description: "**Kerry** - 4.1.\n**Sonja** - 12.2.\n**Christa** - 13.2.\n**Roland** - 22.2.\n**Dennis** - 12.3.\n**Micha3** - 20.3.\n**Ingo** - 8.6.\n**Franz** - 22.7.\n**Nina** - 12.9.\n**Micha** - 10.10.\n**Nadja** - 10.12."
 		}});
 	}
-	
-	// Creating a new job
-	var job1 = new cron.CronJob({
-		cronTime: '* * * * *',
-		onTick: function() {
-			var channel = client.channels.find('name', 'bot-spielwiese');
-			channel.send("<@219918618278756352>, alles Gute zum Geburtstag.");
-		},
-		start: false
-	});
-	job1.start();
 });
 
 //function wishesTo(name) {
