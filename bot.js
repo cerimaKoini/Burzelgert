3db5c0ad17b50998c6887781541a2a99447819ca
cerimaKoini <cerima.koini@gmx.at>
2018-01-11 04:27:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -75,25 +75,25 @@ var birthdays = [
 var testdates = [
 	{
 		UserId: "219918618278756352",
-		Cron: '0 8 11 0 *',
+		Cron: '0 8 11 1 *',
 		Day: 10,
 		Month: 12
 	},
 	{
         	UserId: "219918618278756352",
-		Cron: '24 4 11 0 *',
+		Cron: '30 4 11 1 *',
 		Day: 10,
 		Month: 12
 	},
 	{
         	UserId: "219918618278756352",
-		Cron: '55 3 11 0 *',
+		Cron: '55 4 11 1 *',
 		Day: 10,
 		Month: 12
 	},
 	{
         	UserId: "219918618278756352",
-		Cron: '12 4 11 0 *',
+		Cron: '40 4 11 1 *',
 		Day: 10,
 		Month: 12
 	}
